2562d6d80cc032b4e0f6ca7b5f2b2af588214bcd
Mark Wolfe <mark@wolfe.id.au>
2016-08-16 10:05:38 +1000

    Added support for ansible to manage API gateway / cloudformation.

---
+++ ansible/ansible.go
@@ -0,0 +1,89 @@
+// Package ansible proxies ansible commands.
+package ansible
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/apex/apex/function"
+	"github.com/apex/log"
+)
+
+// Dir in which Terraform configs are stored
+const Dir = "ansible"
+
+// Proxy is a wrapper around Terraform commands.
+type Proxy struct {
+	Functions   []*function.Function
+	Environment string
+	Region      string
+	Role        string
+}
+
+// Run terraform command in infrastructure directory.
+func (p *Proxy) Run(args ...string) error {
+
+	args = append(args, p.functionVars()...)
+	args = append(args, p.ansibleArgs()...)
+
+	log.WithFields(log.Fields{
+		"args": args,
+	}).Debug("ansible")
+
+	cmd := exec.Command("ansible-playbook", args...)
+	cmd.Env = append(os.Environ(), fmt.Sprintf("AWS_REGION=%s", p.Region))
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = filepath.Join(Dir)
+
+	return cmd.Run()
+}
+
+// functionVars returns the function ARN's as ansibles -e arguments.
+func (p *Proxy) functionVars() (args []string) {
+	args = append(args, "-e")
+	args = append(args, fmt.Sprintf("aws_region=%s", p.Region))
+
+	args = append(args, "-e")
+	args = append(args, fmt.Sprintf("apex_environment=%s", p.Environment))
+
+	if p.Role != "" {
+		args = append(args, "-e")
+		args = append(args, fmt.Sprintf("apex_function_role=%s", p.Role))
+	}
+
+	for _, fn := range p.Functions {
+		config, err := fn.GetConfig()
+		if err != nil {
+			log.Debugf("can't fetch function config: %s", err.Error())
+			continue
+		}
+
+		args = append(args, "-e")
+		args = append(args, fmt.Sprintf("apex_function_%s=%s", fn.Name, *config.Configuration.FunctionArn))
+	}
+
+	return args
+}
+
+// ansibleArgs returns the ansible args to run locally
+func (p *Proxy) ansibleArgs() (args []string) {
+	return append(args, []string{"-i", "localhost,", "-c", "local", "apex-playbook.yml"}...)
+}
+
+// Output fetches output variable `name` from ansible.
+func Output(environment, name string) (string, error) {
+	cmd := exec.Command("sh", "-c", fmt.Sprintf("ansible output %s", name))
+	cmd.Dir = filepath.Join(Dir, environment)
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	return strings.Trim(string(out), "\n"), nil
+}
---
+++ cmd/apex/ansible/ansible.go
@@ -0,0 +1,55 @@
+package cf
+
+import (
+	"strings"
+
+	"github.com/apex/apex/ansible"
+	"github.com/apex/apex/cmd/apex/root"
+	"github.com/apex/apex/stats"
+	"github.com/tj/cobra"
+)
+
+// example output.
+const example = `
+    Preview using dry run
+    $ apex ansible apex-playbook.yml --dry-run
+
+    Apply changes
+    $ apex ansible apex-playbook.yml`
+
+// Command config.
+var Command = &cobra.Command{
+	Use:     "ansible",
+	Short:   "ansible management",
+	Example: example,
+	RunE:    run,
+}
+
+// Initialize.
+func init() {
+	root.Register(Command)
+}
+
+// Run command.
+func run(c *cobra.Command, args []string) error {
+	stats.Track("Ansible", nil)
+
+	err := root.Project.LoadFunctions()
+
+	// Hack to prevent initial `apex infra apply` from failing,
+	// as we load functions to expose their ARNs.
+	if err != nil {
+		if !strings.Contains(err.Error(), "Role: zero value") {
+			return err
+		}
+	}
+
+	p := &ansible.Proxy{
+		Functions:   root.Project.Functions,
+		Region:      *root.Session.Config.Region,
+		Environment: root.Project.InfraEnvironment,
+		Role:        root.Project.Role,
+	}
+
+	return p.Run(args...)
+}
